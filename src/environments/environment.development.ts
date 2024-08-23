ad2077e73de4bdaddc74029b66958c5ccd14062f
Jose Barrundia <jose.barrundia@bpx.com>
2024-08-23 06:21:28 -0600

    add environments for local backend and also create another for production --- to be created soon

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const devEnvironment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  // if you want to check the api info you can to to /api-docs url, to check the swagger documentation
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: '', // TODO: deploy to some web hosting
+};
